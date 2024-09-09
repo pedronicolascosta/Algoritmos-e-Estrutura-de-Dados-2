8daf43249003cc64cbb5daa6658a85a3fc220987
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2024-09-09 19:14:03 -0300

    Primeira atividade prática

--- ATIVIDADE 01/exercicio02.c
+++ ATIVIDADE 01/exercicio02.c
@@ -2,47 +2,36 @@
 
 //---atividade prática 01---
 //exercicio 02
-//crie uma matriz para armazenar a pontuação de 3 jogadores em 5 jogos.
-//preencha a matriz com valores inseridos pelo usuário e exiba a pontuação total de cada jogador e qual o jogador vencedor.
-//cada linha incica um jogador, cada coluna sua pontuação.
+//crie uma matriz quadrada onde o usuário fornece o tamanho (n) e depois seja impressa da seguinte maneira:
+// exemplo: se o usuário digitar 5:
+//1 0 0 0 0
+//1 2 0 0 0
+//1 2 3 0 0
+//1 2 3 4 0
+//1 2 3 4 5
 
 #include <stdio.h>
 #include <locale.h>
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	int pontos[3][5], linha, coluna, jogadores[3]={0,0,0}, i;
+	int n, i, j;
 	
-	for(linha=0; linha <3; linha++){
-		printf("\n---PONTOS DO JOGADOR %d---\n", linha+1);
-		for(coluna=0; coluna <5; coluna++){
-			printf("Digite o ponto do %dº jogo do %dº jogador: ", coluna+1, linha+1);
-			scanf("%d", &pontos[linha][coluna]);
-		}
-		for(i=0; i<5; i++){
-			jogadores[linha] = jogadores[linha] + pontos[linha][i];
-		}
-	}
+	printf("Digite o tamanho da matriz quadrada: ");
+	scanf("%d", &n);
 	
-	printf("\nPontuação dos Jogadores\n");
-	for(linha=0; linha<3; linha++){
-		printf("Jogador %d: %d pontos\n", linha+1, jogadores[linha]);
-	}
+	int matriz[n][n];
 	
-	if(jogadores[0] > jogadores[1]){
-		if(jogadores[0] > jogadores[2]){
-			printf("Vencedor: Jogador 1");
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if(j <= i){
+				matriz[i][j] = j+1;
+			}
+			else{
+				matriz[i][j] = 0;
+			}
+			printf("%d ", matriz[i][j]);
 		}
-		else{
-			printf("Vencedor: Jogador 3");
-		}
+		printf("\n");
 	}
-	else{
-		if(jogadores[1] > jogadores[2]){
-			printf("Vencedor: Jogador 2");
-		}
-		else{
-			printf("Vencedor: Jogador 3");
-		}
-	}	
 }
